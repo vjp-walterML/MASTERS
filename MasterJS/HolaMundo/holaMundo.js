8ee0190d69f622483720596279569585b419f35a
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-01-05 10:54:00 +0100

    hm

---
+++ MasterJS/HolaMundo/holaMundo.js
@@ -0,0 +1,5 @@
+/* document.oncontextmenu = function(){return false;}  ---ESTO ELIMINA EL BOTON DERECHO*/
+alert("Esto es una alerta");
+document.write("<h1>Hola mundo</h1>");
+
+console.log("Muestra esto en la consola"); 
