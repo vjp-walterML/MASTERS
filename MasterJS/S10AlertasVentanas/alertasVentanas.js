34663db1b2201ff235f4d254fefa40211710b72b
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-01-07 12:45:15 +0100

    S10

---
+++ MasterJS/S10AlertasVentanas/alertasVentanas.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//ALERTA
+alert("Esta es mi alerta!");
+
+//Alertas de CONFIRMACION
+var resultado = confirm("¿Estas seguro de seguir continuar?"); //Si hacemos un log nos devuelve true o false
+console.log(resultado);
+
+//VENTANA DE INGRESO DE DATOS
+var mi_resultado = prompt("¿Que edad tienes?",18);
+console.log(mi_resultado);
