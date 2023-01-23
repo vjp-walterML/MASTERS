88297a8f3ec822ecff9a41f1869998706ad3b9a0
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-01-23 10:33:17 +0100

    oploj

--- MasterJava/PRUEBAS/src/Prueba.java
+++ MasterJava/PRUEBAS/src/Prueba.java
@@ -2,7 +2,15 @@ import javax.swing.*;
 
 public class Prueba {
     public static void main(String[] args) {
-        int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor, introduzca un numero: "));
-        JOptionPane.showMessageDialog(null,String.valueOf(num));
+        //int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor, introduzca un numero: "));
+        // JOptionPane.showMessageDialog(null,String.valueOf(num));
+        String curso = "PGRMC";
+        String curso2 = new String("PGRMC");
+        String curso3 = "PGRMC";
+
+
+        System.out.println(curso==curso2);//false
+        System.out.println(curso.equals(curso2));//true
+        System.out.println(curso==curso3);//false
     }
 }
