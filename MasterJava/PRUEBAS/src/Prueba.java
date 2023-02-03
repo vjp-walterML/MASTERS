7c3f59ce915db3ceb9d40861f044c206bb1a36f6
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-02-03 20:37:00 +0100

    p

--- MasterJava/PRUEBAS/src/Prueba.java
+++ MasterJava/PRUEBAS/src/Prueba.java
@@ -4,13 +4,15 @@ public class Prueba {
     public static void main(String[] args) {
         //int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor, introduzca un numero: "));
         // JOptionPane.showMessageDialog(null,String.valueOf(num));
-        String curso = "PGRMC";
-        String curso2 = new String("PGRMC");
-        String curso3 = "PGRMC";
+        int a = 1;
+        int b = 5;
+
+        String resultado = 1>5?"esMayor":"esMenor";
+
+        System.out.println(resultado);
+
+         int numero= Integer.parseInt(JOptionPane.showInputDialog("HOLA INGRESA UN NUMERO:"));
 
 
-        System.out.println(curso==curso2);//false
-        System.out.println(curso.equals(curso2));//true
-        System.out.println(curso==curso3);//false
     }
 }
