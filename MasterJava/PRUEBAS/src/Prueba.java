131a2a28827edcd80316bcee4987e5796022f7a5
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-02-21 10:34:28 +0100

    prueba

--- MasterJava/PRUEBAS/src/Prueba.java
+++ MasterJava/PRUEBAS/src/Prueba.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 public class Prueba {
+
     public static void main(String[] args) {
         //int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor, introduzca un numero: "));
         // JOptionPane.showMessageDialog(null,String.valueOf(num));
