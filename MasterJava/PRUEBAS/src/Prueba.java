dd15e695b12e5a0042629979b2c8ff931682f98c
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-02-23 16:44:24 +0100

    prueba

--- MasterJava/PRUEBAS/src/Prueba.java
+++ MasterJava/PRUEBAS/src/Prueba.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.awt.font.FontRenderContext;
+import java.util.ArrayList;
 
 public class Prueba {
 
     public static void main(String[] args) {
-        //int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor, introduzca un numero: "));
+        /*/int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor, introduzca un numero: "));
         // JOptionPane.showMessageDialog(null,String.valueOf(num));
         int a = 1;
         int b = 5;
@@ -12,8 +14,27 @@ public class Prueba {
 
         System.out.println(resultado);
 
-         int numero= Integer.parseInt(JOptionPane.showInputDialog("HOLA INGRESA UN NUMERO:"));
+         int numero= Integer.parseInt(JOptionPane.showInputDialog("HOLA INGRESA UN NUMERO:"));*/
 
+        ArrayList<Character> lRuta = new ArrayList<>();
+        char[] ruta = {'S', 'S', 'O', 'S'};
 
+        // RELLENO LRUTA
+        for (int i = 0; i < ruta.length; i++) {
+            lRuta.add(ruta[i]);
+        }
+         //PRUEBA
+        for (int i = 0; i < lRuta.size(); i++) {
+            mostrarLruta(lRuta);
+            System.out.println("----------------------------");
+            lRuta.remove(0);
+        }
+
+    }
+
+    public static void mostrarLruta(ArrayList<Character> lRuta){
+        for (int i = 0; i < lRuta.size(); i++) {
+            System.out.println("Pos "+i+": "+lRuta.get(i));
+        }
     }
 }
