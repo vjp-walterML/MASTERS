27ccfeaf35497640223af4c6cd7ba25cb782525b
1DAW - Walter Martin Lopes <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2023-04-21 16:21:33 +0200

    casa

--- MasterJava/PRUEBAS/src/Prueba.java
+++ MasterJava/PRUEBAS/src/Prueba.java
@@ -1,14 +1,32 @@
-import javax.swing.*;
-import java.awt.font.FontRenderContext;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Prueba {
 
-    public static void main(String[] args) {
-        Set<Integer> prueba = new TreeSet<>();
+    public static void main(String[] args) throws IOException {
+        File directorio = new File("C:\\Users\\Walter\\Desktop\\PRUEBAJEJE");
+        File archivoPrueba = new File(directorio, "prueba.txt");
+        if (!directorio.exists()) {
+            directorio.mkdir();
+        } else {
+            System.out.println("El directorio existe, voy a proceder a crear el documento de texto...");
+        }
 
+        if(!archivoPrueba.exists()){
+            archivoPrueba.createNewFile();
+            System.out.println("Archivo creado con exito.");
+        } else{
+            System.out.println("El archivo ya existia.");
+        }
+
+        FileWriter fw = new FileWriter(archivoPrueba);
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.println("HOLA ESTOY ESCRIBIENDO");
+
+        pw.close();
+        fw.close();
     }
 }
